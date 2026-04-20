d97de99f2a54e46a2ae2a9d995fcbfa04d07d584
Gabrielvis1 <gabrielvis1214ar@gmail.com>
2026-04-20 16:38:38 -0300

    2004261638

---
+++ Py/dia 5/crear_funciones.py
@@ -0,0 +1,6 @@
+""" Aprender a crear funciones en Python. """
+def saludar(nombre):
+    """ Función que saluda a una persona. """
+    print(f"Hola, {nombre}!")
+
+saludar("Elvis")
---
+++ Py/dia 5/return.py
@@ -0,0 +1,7 @@
+""" Funcion que devuelve un valor """
+def multiplicar(a, b):
+    """ Función que multiplica dos números. """
+    return a * b
+
+resultado = multiplicar(5, 3)
+print(f"El resultado de la multiplicación es: {resultado}")
